093b5da09ce9334afb17fb1fb522c2be94f26d17
Jayhan Sim <jayhan2003@gmail.com>
2015-05-15 15:52:00 +0800

    update responsive almost complete

--- author.php
+++ author.php
@@ -5,12 +5,15 @@
 			<div class="content__left">
 				<?php if (have_posts()): the_post(); ?>
 					<?php if ( get_the_author_meta('description')) : ?>
-					<div class"author">
+					<div class="block__author">
 						<div class="author__image">
 							<?php echo get_avatar(get_the_author_meta('user_email')); ?>
 						</div>
-						<h2><?php echo get_the_author() ; ?></h2>
-						<?php echo wpautop( get_the_author_meta('description') ); ?>
+						<div class="author__info">
+							<h2><?php echo get_the_author() ; ?></h2>
+							<?php echo wpautop( get_the_author_meta('description') ); ?>
+						</div>
+
 					</div>
 					<?php endif; ?>
 				<?php endif; ?>
--- inc/share.php
+++ inc/share.php
@@ -5,25 +5,25 @@
   		<li class="share__fb">
   			<a href="//www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
   				<i class="icon icon__share icon__share--fb"></i>
-  				Share <span></span>
+  				<b>Share</b>
   				</a>
   		</li><!--
   		--><li class="share__tw">
   			<a href="https://twitter.com/share?text=<?php the_title(); ?>&via=iMoneyMY&url=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
   				<i class="icon icon__share icon__share--tw"></i>
-  				Tweet <span></span>
+  				<b>Tweet</b>
   			</a>
   		</li><!--
   		--><li class="share__email">
   			<a href="mailto:?subject=<?php the_title(); ?>&amp;body=Check this article on iMoney <?php the_permalink(); ?>">
   				<i class="icon icon__share icon__share--mail"></i>
-  				Email
+  				<b>Email</b>
   			</a>
   		</li><!--
-  		--><li class="share__whatsapp">
+  		--><li class="share__wa">
   			<a href="whatsapp://send?text=Read this article on iMoney: <?php the_title(); ?> - <?php the_permalink(); ?>">
   				<i class="icon icon__share icon__share--wa"></i>
-  				WhatsApp
+  				<b>WhatsApp</b>
   			</a>
   		</li>
   	</ul>
